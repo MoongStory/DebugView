3c5745abbdccfd8cb6a9f3548496196ef832c88a
Moong <moongstory@gmail.com>
2022-04-30 13:11:48 +0900

    namespace 범위 변경

--- DebugView/DebugView.cpp
+++ DebugView/DebugView.cpp
@@ -4,189 +4,189 @@
 #include <locale.h>
 #include <strsafe.h>
 
-const std::string MOONG::DEBUG_VIEW::DebugView::TRACE_ = "[TRACE]";
-const std::string MOONG::DEBUG_VIEW::DebugView::DEBUG_ = "[DEBUG]";
-const std::string MOONG::DEBUG_VIEW::DebugView::INFO_ = "[INFO]";
-const std::string MOONG::DEBUG_VIEW::DebugView::WARN_ = "[WARN]";
-const std::string MOONG::DEBUG_VIEW::DebugView::ERROR_ = "[ERROR]";
-const std::string MOONG::DEBUG_VIEW::DebugView::FATAL_ = "[FATAL]";
+const std::string MOONG::DebugView::TRACE_ = "[TRACE]";
+const std::string MOONG::DebugView::DEBUG_ = "[DEBUG]";
+const std::string MOONG::DebugView::INFO_ = "[INFO]";
+const std::string MOONG::DebugView::WARN_ = "[WARN]";
+const std::string MOONG::DebugView::ERROR_ = "[ERROR]";
+const std::string MOONG::DebugView::FATAL_ = "[FATAL]";
 
-const unsigned int MOONG::DEBUG_VIEW::DebugView::max_buf_size_ = 1024;
-std::string MOONG::DEBUG_VIEW::DebugView::delimiter_ = "[MOONG_DEBUG]";
-unsigned int MOONG::DEBUG_VIEW::DebugView::log_level_ = MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE;
+const unsigned int MOONG::DebugView::max_buf_size_ = 1024;
+std::string MOONG::DebugView::delimiter_ = "[MOONG_DEBUG]";
+unsigned int MOONG::DebugView::log_level_ = MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE;
 
-void MOONG::DEBUG_VIEW::DebugView::Trace(const std::string format, ...)
+void MOONG::DebugView::Trace(const std::string format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::TRACE_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::TRACE_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::Trace(const std::wstring format, ...)
+void MOONG::DebugView::Trace(const std::wstring format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::TRACE_, format, arg_ptr);
-		va_end(arg_ptr);
-	}
-}
-
-
-
-void MOONG::DEBUG_VIEW::DebugView::Debug(const std::string format, ...)
-{
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_DEBUG)
-	{
-		va_list arg_ptr;
-
-		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::DEBUG_, format, arg_ptr);
-		va_end(arg_ptr);
-	}
-}
-
-void MOONG::DEBUG_VIEW::DebugView::Debug(const std::wstring format, ...)
-{
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_DEBUG)
-	{
-		va_list arg_ptr;
-
-		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::DEBUG_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::TRACE_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
 
 
-void MOONG::DEBUG_VIEW::DebugView::Info(const std::string format, ...)
+void MOONG::DebugView::Debug(const std::string format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_INFO)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_DEBUG)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::INFO_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::DEBUG_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::Info(const std::wstring format, ...)
+void MOONG::DebugView::Debug(const std::wstring format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_INFO)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_DEBUG)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::INFO_, format, arg_ptr);
-		va_end(arg_ptr);
-	}
-}
-
-
-
-void MOONG::DEBUG_VIEW::DebugView::Warn(const std::string format, ...)
-{
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_WARN)
-	{
-		va_list arg_ptr;
-
-		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::WARN_, format, arg_ptr);
-		va_end(arg_ptr);
-	}
-}
-
-void MOONG::DEBUG_VIEW::DebugView::Warn(const std::wstring format, ...)
-{
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_WARN)
-	{
-		va_list arg_ptr;
-
-		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::WARN_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::DEBUG_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
 
 
-void MOONG::DEBUG_VIEW::DebugView::Error(const std::string format, ...)
+void MOONG::DebugView::Info(const std::string format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_INFO)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::ERROR_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::INFO_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::Error(const std::wstring format, ...)
+void MOONG::DebugView::Info(const std::wstring format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_INFO)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::ERROR_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::INFO_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
 
 
-void MOONG::DEBUG_VIEW::DebugView::Fatal(const std::string format, ...)
+void MOONG::DebugView::Warn(const std::string format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_FATAL)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_WARN)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::FATAL_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::WARN_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::Fatal(const std::wstring format, ...)
+void MOONG::DebugView::Warn(const std::wstring format, ...)
 {
-	if(MOONG::DEBUG_VIEW::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_FATAL)
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_WARN)
 	{
 		va_list arg_ptr;
 
 		va_start(arg_ptr, format);
-		MOONG::DEBUG_VIEW::DebugView::Print(MOONG::DEBUG_VIEW::DebugView::FATAL_, format, arg_ptr);
+		MOONG::DebugView::Print(MOONG::DebugView::WARN_, format, arg_ptr);
 		va_end(arg_ptr);
 	}
 }
 
-const std::string MOONG::DEBUG_VIEW::DebugView::Get_delimiter()
+
+
+void MOONG::DebugView::Error(const std::string format, ...)
 {
-	return MOONG::DEBUG_VIEW::DebugView::delimiter_;
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR)
+	{
+		va_list arg_ptr;
+
+		va_start(arg_ptr, format);
+		MOONG::DebugView::Print(MOONG::DebugView::ERROR_, format, arg_ptr);
+		va_end(arg_ptr);
+	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::setDelimiter(const std::string delimiter)
+void MOONG::DebugView::Error(const std::wstring format, ...)
+{
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR)
+	{
+		va_list arg_ptr;
+
+		va_start(arg_ptr, format);
+		MOONG::DebugView::Print(MOONG::DebugView::ERROR_, format, arg_ptr);
+		va_end(arg_ptr);
+	}
+}
+
+
+
+void MOONG::DebugView::Fatal(const std::string format, ...)
+{
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_FATAL)
+	{
+		va_list arg_ptr;
+
+		va_start(arg_ptr, format);
+		MOONG::DebugView::Print(MOONG::DebugView::FATAL_, format, arg_ptr);
+		va_end(arg_ptr);
+	}
+}
+
+void MOONG::DebugView::Fatal(const std::wstring format, ...)
+{
+	if(MOONG::DebugView::getLogLevel() <= MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_FATAL)
+	{
+		va_list arg_ptr;
+
+		va_start(arg_ptr, format);
+		MOONG::DebugView::Print(MOONG::DebugView::FATAL_, format, arg_ptr);
+		va_end(arg_ptr);
+	}
+}
+
+const std::string MOONG::DebugView::Get_delimiter()
+{
+	return MOONG::DebugView::delimiter_;
+}
+
+void MOONG::DebugView::setDelimiter(const std::string delimiter)
 {
 	if (delimiter.empty())
 	{
-		MOONG::DEBUG_VIEW::DebugView::delimiter_ = "[MOONG_DEBUG]";
+		MOONG::DebugView::delimiter_ = "[MOONG_DEBUG]";
 	}
 	else
 	{
-		MOONG::DEBUG_VIEW::DebugView::delimiter_ = delimiter;
+		MOONG::DebugView::delimiter_ = delimiter;
 	}
 }
 
-void MOONG::DEBUG_VIEW::DebugView::setDelimiter(const std::wstring wDelimiter)
+void MOONG::DebugView::setDelimiter(const std::wstring wDelimiter)
 {
 	const size_t delimiter_size = (wDelimiter.length() + 1) * 2;
 	char* delimiter = new char[delimiter_size];
@@ -197,28 +197,28 @@ void MOONG::DEBUG_VIEW::DebugView::setDelimiter(const std::wstring wDelimiter)
 	wcstombs(delimiter, wDelimiter.c_str(), delimiter_size);
 #endif
 
-	MOONG::DEBUG_VIEW::DebugView::setDelimiter(delimiter);
+	MOONG::DebugView::setDelimiter(delimiter);
 
 	delete[] delimiter;
 }
 
-unsigned int MOONG::DEBUG_VIEW::DebugView::getLogLevel()
+unsigned int MOONG::DebugView::getLogLevel()
 {
-	return MOONG::DEBUG_VIEW::DebugView::log_level_;
+	return MOONG::DebugView::log_level_;
 }
 
-void MOONG::DEBUG_VIEW::DebugView::setLogLevel(unsigned int log_level)
+void MOONG::DebugView::setLogLevel(unsigned int log_level)
 {
-	MOONG::DEBUG_VIEW::DebugView::log_level_ = log_level;
+	MOONG::DebugView::log_level_ = log_level;
 
 	if (log_level < MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_TRACE || log_level > MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_FATAL)
 	{
-		MOONG::DEBUG_VIEW::DebugView::Fatal("Log Level 설정값이 잘못되어 Log Level이 Error Level로 초기화 됩니다.");
-		MOONG::DEBUG_VIEW::DebugView::log_level_ = MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR;
+		MOONG::DebugView::Fatal("Log Level 설정값이 잘못되어 Log Level이 Error Level로 초기화 됩니다.");
+		MOONG::DebugView::log_level_ = MOONG::DEBUG_VIEW::LOG_LEVEL::LEVEL_ERROR;
 	}
 	else
 	{
-		MOONG::DEBUG_VIEW::DebugView::log_level_ = log_level;
+		MOONG::DebugView::log_level_ = log_level;
 	}
 }
 
@@ -226,13 +226,13 @@ void MOONG::DEBUG_VIEW::DebugView::setLogLevel(unsigned int log_level)
 
 
 
-void MOONG::DEBUG_VIEW::DebugView::Print(const std::string token, const std::string format, va_list arg_ptr)
+void MOONG::DebugView::Print(const std::string token, const std::string format, va_list arg_ptr)
 {
-	char build_string[MOONG::DEBUG_VIEW::DebugView::max_buf_size_] = { 0 };
+	char build_string[MOONG::DebugView::max_buf_size_] = { 0 };
 
-	StringCchVPrintfA(build_string, MOONG::DEBUG_VIEW::DebugView::max_buf_size_, format.c_str(), arg_ptr);
+	StringCchVPrintfA(build_string, MOONG::DebugView::max_buf_size_, format.c_str(), arg_ptr);
 
-	std::string debug_string(MOONG::DEBUG_VIEW::DebugView::Get_delimiter());
+	std::string debug_string(MOONG::DebugView::Get_delimiter());
 	debug_string += " ";
 	debug_string += token;
 	debug_string += " ";
@@ -241,11 +241,11 @@ void MOONG::DEBUG_VIEW::DebugView::Print(const std::string token, const std::str
 	OutputDebugStringA(debug_string.c_str());
 }
 
-void MOONG::DEBUG_VIEW::DebugView::Print(const std::string token, const std::wstring format, va_list arg_ptr)
+void MOONG::DebugView::Print(const std::string token, const std::wstring format, va_list arg_ptr)
 {
-	wchar_t build_string[MOONG::DEBUG_VIEW::DebugView::max_buf_size_] = { 0 };
+	wchar_t build_string[MOONG::DebugView::max_buf_size_] = { 0 };
 
-	StringCchVPrintfW(build_string, MOONG::DEBUG_VIEW::DebugView::max_buf_size_, format.c_str(), arg_ptr);
+	StringCchVPrintfW(build_string, MOONG::DebugView::max_buf_size_, format.c_str(), arg_ptr);
 
 	size_t convert_string_size = (wcslen(build_string) + 1) * 2;
 	char* convert_string = new char[convert_string_size];
@@ -257,7 +257,7 @@ void MOONG::DEBUG_VIEW::DebugView::Print(const std::string token, const std::wst
 	wcstombs(convert_string, build_string, convert_string_size);
 #endif
 
-	std::string debug_string(MOONG::DEBUG_VIEW::DebugView::Get_delimiter());
+	std::string debug_string(MOONG::DebugView::Get_delimiter());
 	debug_string += " ";
 	debug_string += token;
 	debug_string += " ";
--- DebugView/DebugView.h
+++ DebugView/DebugView.h
@@ -22,57 +22,57 @@ namespace MOONG
 			static const int LEVEL_ERROR = 4;	// 일반 에러가 일어났을 때 사용한다.
 			static const int LEVEL_FATAL = 5;	// 가장 크리티컬한 에러가 일어났을 때 사용한다.
 		}
-
-		class DebugView
-		{
-		public:
-		protected:
-		private:
-			static const std::string TRACE_;
-			static const std::string DEBUG_;
-			static const std::string INFO_;
-			static const std::string WARN_;
-			static const std::string ERROR_;
-			static const std::string FATAL_;
-
-			static const unsigned int max_buf_size_;
-			static std::string delimiter_;
-			static unsigned int log_level_;
-
-
-
-		public:
-			static void Trace(const std::string format, ...);
-			static void Trace(const std::wstring format, ...);
-
-			static void Debug(const std::string format, ...);
-			static void Debug(const std::wstring format, ...);
-
-			static void Info(const std::string format, ...);
-			static void Info(const std::wstring format, ...);
-
-			static void Warn(const std::string format, ...);
-			static void Warn(const std::wstring format, ...);
-
-			static void Error(const std::string format, ...);
-			static void Error(const std::wstring format, ...);
-
-			static void Fatal(const std::string format, ...);
-			static void Fatal(const std::wstring format, ...);
-
-
-
-			static const std::string Get_delimiter();
-			static void setDelimiter(const std::string delimiter);
-			static void setDelimiter(const std::wstring wDelimiter);
-
-			static unsigned int getLogLevel();
-			static void setLogLevel(unsigned int log_level);
-		private:
-			static void Print(const std::string token, const std::string format, va_list arg_ptr);
-			static void Print(const std::string token, const std::wstring format, va_list arg_ptr);
-		};
 	}
+	
+	class DebugView
+	{
+	public:
+	protected:
+	private:
+		static const std::string TRACE_;
+		static const std::string DEBUG_;
+		static const std::string INFO_;
+		static const std::string WARN_;
+		static const std::string ERROR_;
+		static const std::string FATAL_;
+		
+		static const unsigned int max_buf_size_;
+		static std::string delimiter_;
+		static unsigned int log_level_;
+		
+		
+		
+	public:
+		static void Trace(const std::string format, ...);
+		static void Trace(const std::wstring format, ...);
+		
+		static void Debug(const std::string format, ...);
+		static void Debug(const std::wstring format, ...);
+		
+		static void Info(const std::string format, ...);
+		static void Info(const std::wstring format, ...);
+		
+		static void Warn(const std::string format, ...);
+		static void Warn(const std::wstring format, ...);
+		
+		static void Error(const std::string format, ...);
+		static void Error(const std::wstring format, ...);
+		
+		static void Fatal(const std::string format, ...);
+		static void Fatal(const std::wstring format, ...);
+		
+		
+		
+		static const std::string Get_delimiter();
+		static void setDelimiter(const std::string delimiter);
+		static void setDelimiter(const std::wstring wDelimiter);
+		
+		static unsigned int getLogLevel();
+		static void setLogLevel(unsigned int log_level);
+	private:
+		static void Print(const std::string token, const std::string format, va_list arg_ptr);
+		static void Print(const std::string token, const std::wstring format, va_list arg_ptr);
+	};
 }
 
 #endif _DEBUG_VIEW_H_
